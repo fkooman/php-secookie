2817dbed998f93ae0641b5e9433226f3abe257d7
François Kooman <fkooman@tuxed.net>
2017-08-18 23:09:30 +0200

    remove old test

--- tests/CookieTest.php
+++ tests/CookieTest.php
@@ -59,22 +59,6 @@ class CookieTest extends PHPUnit_Framework_TestCase
         );
     }
 
-//    public function testReplaceCookie()
-//    {
-//        $t = new TestHeader();
-//        $c = new Cookie([], $t);
-//        $c->set('foo', 'bar');
-//        $c->set('bar', 'baz');
-//        $c->replace('foo', '123');
-//        $this->assertSame(
-//            [
-//                'Set-Cookie: bar=baz; Secure; HttpOnly; SameSite=Strict',
-//                'Set-Cookie: foo=123; Secure; HttpOnly; SameSite=Strict',
-//            ],
-//            $t->ls()
-//        );
-//    }
-
     public function testAttributeValues()
     {
         $t = new TestHeader();
